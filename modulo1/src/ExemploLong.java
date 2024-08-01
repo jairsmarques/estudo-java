5f0c8b8d9d5d35ee82b67f5d7879c391cd963118
Jair de Sousa <jairsmarques1126@gmail.com>
2024-08-01 18:34:44 -0300

    adicionando código de exemplo de variável do tipo long

---
+++ modulo1/src/ExemploLong.java
@@ -0,0 +1,15 @@
+
+public class ExemploLong {
+
+	public static void main(String[] args) {
+
+		long populacaoOsasco = 699_097;
+		
+		long populacaoOsasco2 = 2_147_483_648L;// tem que colocar L no final
+
+		System.out.println(populacaoOsasco);
+		System.out.println(populacaoOsasco2);
+
+	}
+
+}
