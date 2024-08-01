467f34d7ef42ed5ff1bf283c8b4579d247f525b8
Jair de Sousa <jairsmarques1126@gmail.com>
2024-08-01 18:23:18 -0300

    adicionando o código de exemplo de variáves do tipo byte e short

---
+++ modulo1/src/ExemploByteShort.java
@@ -0,0 +1,16 @@
+
+public class ExemploByteShort {
+
+	public static void main(String[] args) {
+
+		byte idade = 127;
+		// byte idade2 = 128; -> não compila
+		short quantidadeEstoque = 32767;// -> valor máximo
+		// short quantidadeEstoque2 = 32768;// -> não compila
+
+		System.out.println(idade);
+		System.out.println(quantidadeEstoque);
+
+	}
+
+}
