c5a1f11a16075d69a9aa866e1362d21615484f47
Jair de Sousa <jairsmarques1126@gmail.com>
2024-08-01 18:18:23 -0300

    adicionando código de exmplo de variável do tipo char

---
+++ modulo1/src/ExemploChar.java
@@ -0,0 +1,17 @@
+
+public class ExemploChar {
+
+	public static void main(String[] args) {
+
+		// char inicialNome = "J"; -> não compila
+		char inicialNome = 'J';
+		char tipoCliente = '2';
+		char simbolo = '@';
+		
+		System.out.println(inicialNome);
+		System.out.println(tipoCliente);
+		System.out.println(simbolo);
+
+	}
+
+}
